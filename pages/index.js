3fb6f5727ccd0e972de4f68745ebb5c0d409faeb
Peter Durham <peterdurham@protonmail.com>
2020-05-25 13:52:45 -0700

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,268 @@
-import Head from 'next/head'
+import React, { useState, useEffect } from "react";
+import { useSpring, animated as a } from "react-spring";
 
 export default function Home() {
+  const [options, setOptions] = useState(null);
+  const [highScore, setHighScore] = useState(0);
+
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div>
+      <div className="container">
+        <h1>Memory Game</h1>
+        <div>High Score: {highScore}</div>
+        <div>
+          {options === null ? (
+            <>
+              <button onClick={() => setOptions(12)}>Easy</button>
+              <button onClick={() => setOptions(18)}>Medium</button>
+              <button onClick={() => setOptions(24)}>Hard</button>
+            </>
+          ) : (
+            <>
+              <button
+                onClick={() => {
+                  const prevOptions = options;
+                  setOptions(null);
+                  setTimeout(() => {
+                    setOptions(prevOptions);
+                  }, 5);
+                }}
+              >
+                Start Over
+              </button>
+              <button onClick={() => setOptions(null)}>Main Menu</button>
+            </>
+          )}
         </div>
-      </main>
+      </div>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
+      {options ? (
+        <MemoryGame options={options} />
+      ) : (
+        <h2>Choose a difficulty to begin!</h2>
+      )}
 
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
+      <style jsx global>
+        {`
+          body {
+            text-align: center;
+            font-family: -apple-system, sans-serif;
           }
-        }
-      `}</style>
+          .container {
+            width: 1060px;
+            margin: 0 auto;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+          }
+          #cards {
+            width: 1060px;
+            margin: 0 auto;
+            display: flex;
+            flex-wrap: wrap;
+          }
+          .card {
+            width: 160px;
+            height: 160px;
+            margin-bottom: 20px;
+          }
+          .card:not(:nth-child(6n)) {
+            margin-right: 20px;
+          }
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
+          .c {
+            position: absolute;
+            max-width: 160px;
+            max-height: 160px;
+            width: 50ch;
+            height: 50ch;
+            cursor: pointer;
+            will-change: transform, opacity;
+          }
 
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+          .front,
+          .back {
+            background-size: cover;
+          }
+
+          .back {
+            background-image: url(https://images.unsplash.com/photo-1544511916-0148ccdeb877?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1901&q=80i&auto=format&fit=crop);
+          }
+
+          .front {
+            background-image: url(https://images.unsplash.com/photo-1540206395-68808572332f?ixlib=rb-1.2.1&w=1181&q=80&auto=format&fit=crop);
+          }
+        `}
+      </style>
     </div>
-  )
+  );
+}
+
+function MemoryGame({ options, setOptions, setHighScore }) {
+  const [game, setGame] = useState([]);
+  const [flippedCount, setFlippedCount] = useState(0);
+  const [flippedIndexes, setFlippedIndexes] = useState([]);
+  const colors = [
+    "#ecdb54",
+    "#e34132",
+    "#6ca0dc",
+    "#944743",
+    "#dbb2d1",
+    "#ec9787",
+    "#00a68c",
+    "#645394",
+    "#6c4f3d",
+    "#ebe1df",
+    "#bc6ca7",
+    "#bfd833",
+  ];
+
+  useEffect(() => {
+    const newGame = [];
+    for (let i = 0; i < options / 2; i++) {
+      const firstOption = {
+        id: 2 * i,
+        colorId: i,
+        color: colors[i],
+        flipped: false,
+      };
+      const secondOption = {
+        id: 2 * i + 1,
+        colorId: i,
+        color: colors[i],
+        flipped: false,
+      };
+
+      newGame.push(firstOption);
+      newGame.push(secondOption);
+    }
+
+    const shuffledGame = newGame.sort(() => Math.random() - 0.5);
+    setGame(shuffledGame);
+  }, []);
+
+  useEffect(() => {
+    console.log(game);
+    const finished = !game.some((card) => !card.flipped);
+    if (finished && game.length > 0) {
+      setTimeout(() => {
+        const newGame = confirm("YOU WIN! NEW GAME?");
+        console.log(newGame);
+
+        // if confirm, show menu options
+        // set high score
+      }, 500);
+    }
+  }, [game]);
+
+  if (flippedIndexes.length === 2) {
+    const match =
+      game[flippedIndexes[0]].colorId === game[flippedIndexes[1]].colorId;
+
+    if (match) {
+      const newGame = [...game];
+      newGame[flippedIndexes[0]].flipped = true;
+      newGame[flippedIndexes[1]].flipped = true;
+      setGame(newGame);
+
+      const newIndexes = [...flippedIndexes];
+      newIndexes.push(false);
+      setFlippedIndexes(newIndexes);
+    } else {
+      const newIndexes = [...flippedIndexes];
+      newIndexes.push(true);
+      setFlippedIndexes(newIndexes);
+    }
+  }
+
+  if (game.length === 0) return <div>loading...</div>;
+  else {
+    return (
+      <div id="cards">
+        {game.map((card, index) => (
+          <div className="card" key={index}>
+            <Card
+              id={index}
+              color={card.color}
+              game={game}
+              flippedCount={flippedCount}
+              setFlippedCount={setFlippedCount}
+              flippedIndexes={flippedIndexes}
+              setFlippedIndexes={setFlippedIndexes}
+            />
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+function Card({
+  id,
+  color,
+  game,
+  flippedCount,
+  setFlippedCount,
+  flippedIndexes,
+  setFlippedIndexes,
+}) {
+  const [flipped, set] = useState(false);
+  const { transform, opacity } = useSpring({
+    opacity: flipped ? 1 : 0,
+    transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
+    config: { mass: 5, tension: 500, friction: 80 },
+  });
+
+  useEffect(() => {
+    if (flippedIndexes[2] === true && flippedIndexes.indexOf(id) > -1) {
+      setTimeout(() => {
+        set((state) => !state);
+        setFlippedCount(flippedCount + 1);
+        setFlippedIndexes([]);
+      }, 1000);
+    } else if (flippedIndexes[2] === false && id === 0) {
+      setFlippedCount(flippedCount + 1);
+      setFlippedIndexes([]);
+    }
+  }, [flippedIndexes]);
+
+  const onCardClick = () => {
+    if (!game[id].flipped && flippedCount % 3 === 0) {
+      set((state) => !state);
+      setFlippedCount(flippedCount + 1);
+      const newIndexes = [...flippedIndexes];
+      newIndexes.push(id);
+      setFlippedIndexes(newIndexes);
+    } else if (
+      flippedCount % 3 === 1 &&
+      !game[id].flipped &&
+      flippedIndexes.indexOf(id) < 0
+    ) {
+      set((state) => !state);
+      setFlippedCount(flippedCount + 1);
+      const newIndexes = [...flippedIndexes];
+      newIndexes.push(id);
+      setFlippedIndexes(newIndexes);
+    }
+  };
+
+  return (
+    <div onClick={onCardClick}>
+      <a.div
+        className="c back"
+        style={{
+          opacity: opacity.interpolate((o) => 1 - o),
+          transform,
+        }}
+      />
+      <a.div
+        className="c front"
+        style={{
+          opacity,
+          transform: transform.interpolate((t) => `${t} rotateX(180deg)`),
+          background: color,
+        }}
+      />
+    </div>
+  );
 }
